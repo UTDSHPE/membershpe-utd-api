ef696719b1b2fa4af9a41edf94498ea1aa565c9d
Lerich Osay <lerichyaso@gmail.com>
2024-09-28 00:39:05 -0500

    include checking for paid members

--- server.js
+++ server.js
@@ -18,48 +18,79 @@ const auth = new google.auth.GoogleAuth({
 
 const sheets = google.sheets({ version: 'v4', auth });
 
-// API endpoint
-app.post('/api/members', async (req, res) => {
+// API endpoint to validate members
+app.post('/api/membershpe', async (req, res) => {
   const { firstName, lastName, netID } = req.body;
 
-  const name = `${firstName} ${lastName}`;
-  const altName = `${firstName}, ${lastName}`;
+  const name = `${firstName} ${lastName}`.toLowerCase();
+  const altName = `${firstName}, ${lastName}`.toLowerCase();
 
   if (!name || !netID) {
     return res.status(400).json({ error: 'Name and ID are required' });
   }
 
   try {
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+
+    // request rows from membership form
+    const signUpSheetResponse = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.RESPONSES_GOOGLE_SHEET_ID,
       range: 'Form Responses 1',
     });
+    const signUpSheetRows = signUpSheetResponse.data.values;
+    const signedUp = signUpSheetRows.some(row => (row[2].toLowerCase() === name || row[2].toLowerCase() == altName) && row[6].toLowerCase() === `${netID}@utdallas.edu`);
 
-    const rows = response.data.values;
+    // request rows from paid dues sheet
+    const paidDuesSheetResponse = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.PAID_DUES_GOOGLE_SHEET_ID,
+      range: 'Sheet1',
+    });
+    const paidDuesSheetRows = paidDuesSheetResponse.data.values;
+    const paid = paidDuesSheetRows.some(row => (row[0].toLowerCase() === name || row[0].toLowerCase() == altName) && row[1].toLowerCase() === netID && row[3].toLowerCase() === 'true');
 
-    // inconsistent data input will screw this up
-    const isValid = rows.some(row => (row[2] === name || row[2] == altName) && row[6].toLowerCase() === `${netID}@utdallas.edu`);
-
-    res.json({ isValid });
+    res.json({ signedUp, paid });
   } catch (error) {
-    console.error('Error accessing Google Sheet:', error);
+    console.error('Error accessing member in Google Sheet:', error);
     res.status(500).json({ error: 'Failed to validate user' });
   }
 });
 
-app.get('/api/members', async (req, res) => {
+app.get('/api/responses/rows', async (req, res) => {
   const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    spreadsheetId: process.env.RESPONSES_GOOGLE_SHEET_ID,
     range: 'Form Responses 1',
   });
 
   const rows = response.data.values;
-  res.status(200).json({rows})
+  res.status(200).json({ rows })
 })
 
-app.get('/api/metadata', async (req, res) => {
+app.get('/api/responses/metadata', async (req, res) => {
 
-  const spreadsheetId = process.env.GOOGLE_SHEET_ID;
+  const spreadsheetId = process.env.RESPONSES_GOOGLE_SHEET_ID;
+  try {
+    const sheetMetadata = await sheets.spreadsheets.get({ auth, spreadsheetId });
+
+    res.send(sheetMetadata.data)
+  } catch (error) {
+    console.error('Error accessing Google Sheet:', error);
+    res.status(500).json({ error: 'Failed to fetch metadata' });
+  }
+})
+
+app.get('/api/dues/rows', async (req, res) => {
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.PAID_DUES_GOOGLE_SHEET_ID,
+    range: 'Sheet1',
+  });
+
+  const rows = response.data.values;
+  res.status(200).json({ rows })
+})
+
+
+app.get('/api/dues/metadata', async (req, res) => {
+
+  const spreadsheetId = process.env.PAID_DUES_GOOGLE_SHEET_ID;
   try {
     const sheetMetadata = await sheets.spreadsheets.get({ auth, spreadsheetId });
 
@@ -72,7 +103,7 @@ app.get('/api/metadata', async (req, res) => {
 
 // API checking endpoint
 app.get('/api/hello', async (req, res) => {
-  return res.status(200).json({msg: 'hello world, server is running'})
+  return res.status(200).json({ msg: 'hello world, server is running' })
 })
 
 // Start the server
